674556df21adc8b8d6bc8f9067ce93e5bb2852d4
FRANKFRANK5 <frankkarani146@gmail.com>
2026-04-08 19:34:36 +0000

    Admin access granted to Frank

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,17 @@
 import os
-
-# Detect if we're running via `flask run` and don't monkey patch
-if not os.getenv("FLASK_RUN_FROM_CLI"):
-    from gevent import monkey
-
-    monkey.patch_all()
-
 from CTFd import create_app
+from CTFd.models import Users, db
 
 app = create_app()
 
+# Hii inampa Frank u-Admin kila server inapowaka
+with app.app_context():
+    user = Users.query.filter_by(email="frankkarani280@gmail.com").first()
+    if user:
+        user.type = "admin"
+        db.session.commit()
+        print("Mamlaka ya Admin yamewekwa kwa frankkarani280@gmail.com")
+
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, host="127.0.0.1", port=4000)
+    # Tumia 0.0.0.0 ili Render iweze kuunganisha
+    app.run(debug=True, threaded=True, host="0.0.0.0", port=4000)
